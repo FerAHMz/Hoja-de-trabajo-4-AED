86af91bbf73b212ac9af3372dc04c7ff6ce08870
Fernando Hernandez <ferandree270904@gmail.com>
2024-02-19 09:14:26 -0600

    Creacion de clases
    Estas clases implementan la interfaz stack

---
+++ VectorStack.java
@@ -0,0 +1,35 @@
+import java.util.Vector;
+
+public class VectorStack<T> implements UVGStack<T> {
+    private Vector<T> stack;
+
+    public VectorStack() {
+        this.stack = new Vector<>();
+    }
+
+    @Override
+    public void push(T item) {
+        stack.add(item);
+    }
+
+    @Override
+    public T pop() {
+        if (!isEmpty()) {
+            return stack.remove(stack.size() - 1);
+        }
+        return null; // O manejar de otra manera
+    }
+
+    @Override
+    public T peek() {
+        if (!isEmpty()) {
+            return stack.get(stack.size() - 1);
+        }
+        return null; // O manejar de otra manera
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
